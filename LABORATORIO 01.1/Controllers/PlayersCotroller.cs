7f69353e3cf07defc0c08efccc755014ca4a135a
SergioCabrera <sergiodcr0258@gmail.com>
2021-02-20 07:25:30 -0600

    Realizando ajustes

--- LABORATORIO 01.1/Controllers/PlayersCotroller.cs
+++ LABORATORIO 01.1/Controllers/PlayersCotroller.cs
@@ -25,9 +25,54 @@ namespace LABORATORIO_01._1.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Create(IformCollection player)
-        {
+        //public ActionResult Create()
+        //{
+        //    PlayerM NewPlayer = new PlayerM();
+        //    NewPlayer.Name = player["Name"];
+        //    NewPlayer.LastN = player["LastN"];
+        //    NewPlayer.Club = player["Club"];
+        //    NewPlayer.Position = player["Position"];
+        //    NewPlayer.Income = player["Income"];
+        //    playerslist.Add(NewPlayer);
+        //    ViewBag.UniqueList = playerslist;
+        //    return View("Index");
+        //}
+        //EDIT
+        
 
+
+
+
+        public ActionResult CreateFile(FormCollection collection)
+        {
+            StreamReader streamReader = new StreamReader(collection["path"]);
+            var playerslist = (streamReader.ReadToEnd()).Split('\r');
+
+            for (int i = 0; i < playerslist.Length; i++)
+            {
+                if (playerslist[i][0] == '\r')
+                {
+                    playerslist[i] = playerslist[i].Substring(i);
+                }
+
+            }
+
+            foreach (var details in playerslist)
+            {
+                var detailsList = details.Split(',');
+                PlayerM player1 = new PlayerM
+                {
+                    Name = detailsList[0],
+                    LastN = detailsList[1],
+                    Club = detailsList[2],
+                    Position = detailsList[3],
+                    Income = Convert.ToInt32(detailsList[4]),
+                    ID = Convert.ToInt32(detailsList[5])
+                };
+                //Storage.Instance.playerList.AddFirst(jugador1);
+            }
+            return RedirectToAction("Index");      
         }
-    }
+
+    } 
 }
